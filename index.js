2bdf53348a8801fca2d070f0fd121385a86ff419
yuukimaru <yuuk130516@gmail.com>
2021-09-16 17:17:53 +0900

    修正

--- index.js
+++ index.js
@@ -1,10 +1,80 @@
 'use strict';
 
 // canvas の読み込み
-/* const gameArea = document.getElementById('game-area');
+const gameArea = document.getElementById('game-area');
 const gameArea_ctx = gameArea.getContext('2d');
 
-gameArea_ctx.fillRect(0,0,100,100); */
+gameArea_ctx.font = "13px monospace"
+
+// dealer Area
+gameArea_ctx.fillStyle = "gray";
+gameArea_ctx.fillRect(400, 230, 500, 300);
+
+// user Area
+gameArea_ctx.fillRect(400, 600, 500, 300);
+
+// 画像サイズ
+const dx = 712;
+const dy = 1008;
+
+// TODO choice card が読み込まれた後、new image() が一つ作成される。
+// 画像読み込み
+const srcs = [];
+
+for(let i=1; i<=13; i++){
+  srcs.push(`./trump-image/spade${i}.png`);
+  srcs.push(`./trump-image/crab${i}.png`);
+  srcs.push(`./trump-image/daia${i}.png`);
+  srcs.push(`./trump-image/hart${i}.png`);
+}
+
+const dealer = new Image();
+dealer.src = './trump-image/casino_dealer_man2.png';
+dealer.onload = () => {
+  gameArea_ctx.drawImage(dealer, 450, 10);
+}
+
+
+
+// 画像描画
+function createImage(isUser){
+  if(isUser){
+    const userImages = [];
+
+    for(let i in userCards){
+      let drawUserImage = srcs.find((image) => {
+        return image === `./trump-image/${userCards[i].mark}${userCards[i].value}.png`;
+      });
+      userImages[i] = new Image();
+      userImages[i].src = drawUserImage;
+      userImages[i].onload = () => {
+        gameArea_ctx.drawImage(userImages[i], 150 * i + 500, 700, dx/7, dy/7);
+      }
+    }
+    
+  }else{
+    const dealerImages = [];
+
+    // trump の表
+    const omote = new Image();
+    omote.src = "./trump-image/omote.png";
+
+    for(let i in dealerCards){
+      let drawDealerImage = srcs.find((image) => {
+        return image === `./trump-image/${dealerCards[i].mark}${dealerCards[i].value}.png`;
+      });
+      dealerImages[i] = new Image();
+      dealerImages[i].src = drawDealerImage;
+      dealerImages[i].onload = () => {
+        if(i == 1){
+          gameArea_ctx.drawImage(omote, 150 * i + 500, 280, dx/7, dy/7);
+        }else{
+          gameArea_ctx.drawImage(dealerImages[i], 150 * i + 500, 280, dx/7, dy/7);
+        }
+      }
+    }
+  }
+}
 
 // suffle メソッド
 Array.prototype.shuffle = function() {
@@ -12,7 +82,7 @@ Array.prototype.shuffle = function() {
 }
 
 // デックの作成
-const marks = ["♠", "♣", "♥", "♦"];
+const marks = ["spade", "crab", "hart", "daia"];
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 const decks = new Array;
 
@@ -78,73 +148,115 @@ let nowScore = 0;
 let n = 0;
 let stop = false;
 
-
-// ゲームの実装部分
-function main () {
-  console.log("★☆★☆★☆★☆ ブラックジャックへようこそ! ★☆★☆★☆★☆");
-  console.log('ゲームを開始します');
-  // 1 ターン目
-  choiceCard(n, true);
-  console.log('あなたの引いた1枚目のカードは' + userDisplayCards[n-1].mark + userDisplayCards[n-1].value + 'です。');
-  choiceCard(n, true);
-  console.log('あなたの引いた2枚目のカードは' + userDisplayCards[n-1].mark + userDisplayCards[n-1].value + 'です。');
-  choiceCard(n, false);
-  console.log('ディーラーの引いたカードは' + dealerDisplayCards[n-userCards.length - 1].mark + dealerDisplayCards[n-userCards.length - 1].value + 'です。');
-  choiceCard(n, false);
-  console.log('ディーラーの2枚目のカードは分かりません。');
-  
-  // ユーザからのキーボード入力を取得する Promise を生成する
-  function readUserInput(question) {
-    const readline = require('readline').createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-
-    return new Promise((resolve) => {
-      readline.question(question, (answer) => {
-        resolve(answer);
-        readline.close();
-      });
-    });
-  }
-  (async function stdout() {
-    console.log(`現在のあなたの得点は、${calculate(true)} です。`);
-    while(calculate(true) < 21 && stop === false && gameOver === false){
-      const ques = await readUserInput('カードを引きますか?   y/n  ');
-      if(ques === "y") {
-        choiceCard(n, true);
-        console.log(`あなたの引いた${n-dealerCards.length}枚目のカードは ${userDisplayCards[n-dealerCards.length-1].mark} ${userDisplayCards[n-dealerCards.length-1].value} です。`);
-        console.log(`あなたの得点は、${calculate(true)} です`);
-        if(gameOver === true){ 
-          console.log('あなたの負けです' );
-          return false;
-       }
-      }else if(ques === "n") {
-        stop = true;
-      }else {
-        console.log('無効な入力です。');
-      }
+// ヒット処理 
+function drawCard(isUser) {
+  $("#hit").on('click', () => {
+    choiceCard(n, isUser);
+    gameArea_ctx.clearRect(750, 0, 900, 150);
+    gameArea_ctx.fillText(`現在のあなたの得点は、${calculate(true)} 点です。` , 750, 100);
+    gameArea_ctx.fillText(`カードを引きますか？` , 750, 120);
+    if( isOver(true) ) {
+      Object.seal(userCards);
+      Object.seal(dealerCards);
+      gameArea_ctx.clearRect(750, 0, 900, 150);
+      gameArea_ctx.fillText(`現在のあなたの得点は、${calculate(true)} 点です。` , 750, 100);
+      gameArea_ctx.fillText(`21点を超えたのであなたの負けです` , 750, 120);
     }
-    // TODO ディーラーの処理に移行
-    console.log(`ディーラーの2枚目は、${dealerDisplayCards[1].mark} ${dealerDisplayCards[1].value} でした。`)
-    console.log(`現在のディーラーの得点は、${calculate(false)} です。`);
-    while(calculate(false) < 17) { 
-      choiceCard(n, false);
-      console.log(`ディーラーの引いた${n-userCards.length}枚目のカードは、${dealerDisplayCards[n-userCards.length-1].mark} ${dealerDisplayCards[n-userCards.length-1].value}`);
-      console.log(`ディーラーの現在の得点は、${calculate(false)} 点です`);
-    }
-    
-    // 勝負フェーズ
-    if(calculate(true) > calculate(false)){
-      console.log('あなたの勝ちです');
-    }else if(calculate(true) < calculate(false)){
-      console.log('あなたの負けです')
-    }else {
-      console.log('引き分けです')
-    }
-    
-  })();
+  });
 }
+
+// スタンド処理
+function stopCard() {
+  return new Promise(resolve => {
+    $("#stand").on('click', () => {
+      Object.seal(userCards);
+      resolve();
+    });
+  })
+}
+
+// バーストしたか判定する
+function isOver(isUser) {
+  if(calculate(isUser) > 21){
+    return true;
+  } 
+}
+
+
+
+
+function dealerTurn() {
+    gameArea_ctx.clearRect(750, 0, 900, 150);
+    gameArea_ctx.fillText(`ディーラーの番です。` , 750, 100);
+    gameArea_ctx.fillText(`現在ディーラーの得点は、${calculate(false)}点です。` , 750, 120);
+
+  while(calculate(false) < 17) {
+    choiceCard(n, false);
+    drawDelerSecondImage();
+    gameArea_ctx.clearRect(750, 0, 900, 150);
+    gameArea_ctx.fillText(`ディーラーの番です。` , 750, 100);
+    gameArea_ctx.fillText(`現在ディーラーの得点は、${calculate(false)}点です。` , 750, 120);
+  }
+  if( isOver(false)){
+    gameArea_ctx.clearRect(750, 0, 900, 150);
+    gameArea_ctx.fillText(`ディーラーの得点は、${calculate(false)}点です。` , 750, 100);
+    gameArea_ctx.fillText(`あなたの勝ちです。` , 750, 120);
+  }
+}
+
+// main 部分
+async function main(){
+  // 初期値
+  choiceCard(n, true);
+  choiceCard(n, true);
+  choiceCard(n, false);
+  choiceCard(n, false);
+
+  // your turn
+  gameArea_ctx.fillText(`現在のあなたの得点は、${calculate(true)} 点です。` , 750, 100);
+  gameArea_ctx.fillText(`カードを引きますか？` , 750, 120);
+  drawCard(true);
+  await stopCard();
+
+  //TODO ディーラー処理
+  if(!isOver(true)){
+    drawDelerSecondImage();
+    dealerTurn();
+  }
+
+  // TODO 勝負
+  if (!isOver(false)) {
+    setTimeout(() => {
+      compareCard();
+    }, 2000);
+  }
+}
+
+main();
+
+function drawDelerSecondImage() {
+  const dealerSecond = new Image();
+  dealerSecond.src = `./trump-image/${dealerCards[1].mark}${dealerCards[1].value}.png`;
+  dealerSecond.onload = () =>{
+    gameArea_ctx.drawImage(dealerSecond, 150 + 500, 280, dx/7, dy/7);
+  } 
+}
+
+function compareCard() {
+  gameArea_ctx.clearRect(750, 0, 900, 150);
+  gameArea_ctx.fillText(`ディーラーの得点は、${calculate(false)}点です。` , 750, 100);
+  gameArea_ctx.fillText(`あなたの得点は、${calculate(true)}点です。` , 750, 120);
+
+  if(calculate(true) > calculate(false)){
+    gameArea_ctx.fillText(`あなたの勝ちです。` , 750, 140);
+  }else if(calculate(true) < calculate(false)){
+    gameArea_ctx.fillText(`あなたの負けです。` , 750, 140);
+  }else  {
+    gameArea_ctx.fillText(`引き分けです` , 750, 140);
+  }
+}
+    
+
 
 // 点数計算
 function calculate(isUser) {
@@ -173,7 +285,8 @@ function choiceCard(i, isUser) {
     dealerDisplayCards.push(displayCards[i]);
     dealerCalculateCards.push(calculateCards[i]);
   }
+  createImage(isUser);
   n ++;
 }
 
-main();
+
